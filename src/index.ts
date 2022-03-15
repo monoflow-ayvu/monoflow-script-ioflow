c60a3daadde8de40332fa3d24cc70ea76543e2d4
Fernando Mumbach <fermuch@ayvu.net>
2022-03-15 10:32:09 -0300

    adding config for IO

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
 import * as MonoUtils from "@fermuch/monoutils";
 
+type IORuleMonoflow = {
+  enable: boolean;
+  low: number;
+  high: number;
+  target: string;
+  save: boolean;
+  trigger: boolean;
+  reverse: boolean;
+  action: number;
+  // TODO: reverse_only: boolean
+}
+
+type IORule = {
+  rule: number; // needs to be floored
+  enable: boolean;
+  low: number;
+  high: number;
+  target: string;
+  save: boolean;
+  trigger: boolean;
+  reverse: boolean;
+  action: string; // when syncing needs to be casted to number
+  // TODO: reverse_only: boolean
+}
+
 // based on settingsSchema @ package.json
 type Config = {
+  syncIORules: boolean;
+  rules: IORule[];
 }
 
 const conf = new MonoUtils.config.Config<Config>();
@@ -13,5 +40,29 @@ messages.on('onInit', function() {
   if (foundBle && foundBle != data.BLE_TARGET) {
     env.setData('BLE_TARGET', foundBle);
     platform.log('BLE target changed to: ' + foundBle);
+  }
+
+  if (conf.get('syncIORules', false)) {
+    const rawRules = conf.get('rules', []);
+    const rules = rawRules.reduce((acc, rule) => {
+      const ruleId = Math.floor(rule.rule);
+      acc[ruleId] = {
+        enable: Boolean(rule.enable),
+        low: Number(rule.low),
+        high: Number(rule.high),
+        target: String(rule.target),
+        save: Boolean(rule.save),
+        trigger: Boolean(rule.trigger),
+        reverse: Boolean(rule.reverse),
+        action: Number(rule.action) || 2
+      };
+
+      return acc;
+    }, {} as {
+      [ruleId: number]: IORuleMonoflow
+    });
+
+    platform.log('setting Monoflow Rules to: ' + JSON.stringify(rules));
+    env.setData('MONOFLOW_RULES', rules);
   }
 });
